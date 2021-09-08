7b394aaa78ab159ec03231ecfdb4ad11f7256b38
Myssenberg <90093429+Myssenberg@users.noreply.github.com>
2021-09-08 21:34:42 +0200

    Implemented Hello World (#1)

---
+++ Lecture01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System; //tge same as import in Java
+
+namespace Lecture01 //equivalent to a package - the name of the folder or project you're in
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
